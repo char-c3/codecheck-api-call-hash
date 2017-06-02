4ae8ac4ee5f9d4393961b9e7d3e5f0c79f1465f5
char_c3 <work238fnct@gmail.com>
2017-06-03 06:21:05 +0900

    なぜかfopenしていたのを修正

--- app/Main.php
+++ app/Main.php
@@ -5,8 +5,7 @@ function run ($argc, $argv)
   $url = "http://challenge-server.code-check.io/api/hash";
   foreach ($argv as $index=>$value) {
     $query = "?q=". urlencode($value);
-    $url_handle = fopen($url . $query, "r");
-    $json = file_get_contents($url_handle);
+    $json = file_get_contents($url . $query);
     $data = json_decode($json);
     echo $data->hash, PHP_EOL;
   }
