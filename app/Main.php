131003dbe76c659a7577f7d6d9bab9eb36d09a73
char_c3 <work238fnct@gmail.com>
2017-06-03 05:17:24 +0900

    回答コードを作成

--- app/Main.php
+++ app/Main.php
@@ -2,7 +2,16 @@
 
 function run ($argc, $argv)
 {
+  $url = "http://challenge-server.code-check.io/api/hash";
   foreach ($argv as $index=>$value) {
-    printf("argv[%s]: %s\n", $index, $value);
+    $query = "?q=". urlencode($value);
+    $url_handle = fopen($url . $query, "r");
+    $json = "";
+    while ($gotstr = fread($url_handle, 1024)) {
+        $json .= $gotstr;
+    }
+    $data = json_decode($json);
+    echo $data->hash, PHP_EOL;
   }
 }
+
